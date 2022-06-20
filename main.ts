98591b2c3b6574b1c09fd35f7f629c25e64d2edf
ayame113 <40050810+ayame113@users.noreply.github.com>
2022-06-21 01:37:18 +0900

    try

--- main.ts
+++ main.ts
@@ -1,6 +1,14 @@
 import { serve } from "https://deno.land/std@0.144.0/http/mod.ts";
 import { DB } from "https://deno.land/x/sqlite@v3.4.0/mod.ts";
 
+const f = await Deno.open("./db.sqlite");
+Deno.openSync = function (path: string | URL) {
+  if (path === "./db.sqlite") {
+    return f;
+  }
+  throw new Error("can not open" + path);
+};
+
 const db = new DB("./db.sqlite");
 const rows = db.query("SELECT * FROM people ");
 console.log(rows);
