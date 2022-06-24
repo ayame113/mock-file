a4b46131b111998d6b066e3d176c4e19fff87a75
ayame113 <40050810+ayame113@users.noreply.github.com>
2022-06-24 21:40:00 +0900

    test

--- main.ts
+++ main.ts
@@ -3,8 +3,11 @@ import { DB } from "https://deno.land/x/sqlite@v3.4.0/mod.ts";
 
 import { prepareFile } from "./mod.ts";
 
+import { pathFromURL } from "./src/memory_file.ts";
 // prepare file
 await prepareFile("./db.sqlite");
+
+console.log(pathFromURL("./db.sqlite"));
 
 // read db
 const db = new DB("./db.sqlite", { mode: "read" });
